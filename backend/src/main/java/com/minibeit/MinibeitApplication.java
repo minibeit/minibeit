2404786200d0e64159e53756701b73efb0aa2410
donup3 <donup08@naver.com>
2021-08-22 15:12:46 +0900

    initial commit

---
+++ backend/src/main/java/com/minibeit/MinibeitApplication.java
@@ -0,0 +1,11 @@
+package com.minibeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibeitApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MinibeitApplication.class, args);
+	}
+}

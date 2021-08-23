cd9d547ef812cb52b45e76ade449b0f5ea93f6fb
donup3 <donup08@naver.com>
2021-08-23 16:03:37 +0900

    feat: 학교 전체 조회 기능 구현(#3)

---
+++ backend/src/main/java/com/minibeit/school/dto/SchoolResponse.java
@@ -0,0 +1,22 @@
+package com.minibeit.school.dto;
+
+import com.minibeit.school.domain.School;
+import lombok.*;
+
+public class SchoolResponse {
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class GetList {
+        private Long id;
+        private String name;
+
+        public static SchoolResponse.GetList build(School school) {
+            return SchoolResponse.GetList.builder()
+                    .id(school.getId())
+                    .name(school.getName())
+                    .build();
+        }
+    }
+}
---
+++ backend/src/main/java/com/minibeit/school/web/SchoolController.java
@@ -0,0 +1,22 @@
+package com.minibeit.school.web;
+
+import com.minibeit.school.dto.SchoolResponse;
+import com.minibeit.school.service.SchoolService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/school")
+public class SchoolController {
+    private final SchoolService schoolService;
+
+    @GetMapping("/list")
+    public List<SchoolResponse.GetList> getList() {
+        return schoolService.getList();
+    }
+}

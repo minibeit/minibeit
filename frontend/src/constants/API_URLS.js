532b5ce8d9d098168b5c088d438ff77342eabb53
seol <shs00421@gmail.com>
2021-08-24 14:11:45 +0900

    refactor: api 리팩토링 (#2)

--- frontend/src/constants/API_URLS.js
+++ frontend/src/constants/API_URLS.js
@@ -1,15 +1,16 @@
+//환경변수 파일로 따로 분리해야할 듯
+const BAISIC_URL = "http://3.36.95.15:8080";
+
 const API_URLS = {
-    ///백엔드 맞추기!!! 수정전
-    LOGOUT: 'http://3.36.95.15:8080/api/user/logout',
-    PROFILES: '/account/profiles/',
-    USERINFO: '/account/userinfo/',
-    SIGNUP: 'http://3.36.95.15:8080/api/user/signup',
-    LOGIN: 'http://3.36.95.15:8080/api/user/login',
-    TOKEN_OBTAIN: '/account/token/obtain/',
-    TOKEN_REFRESH: '/account/token/refresh/',
-    TOKEN_BLACKLIST: '/account/token/blacklist/',
-    FEED_NEW : 'http://3.36.95.15:8080/api/board',
-  };
-  
-  export default API_URLS;
-  
+  ///백엔드 맞추기!!! 수정전
+  LOGIN_KAKAO: BAISIC_URL + "/oauth2/authorization/kakao",
+  SIGNUP: BAISIC_URL + "/api/user/signup",
+  TOKEN_REFRESH: BAISIC_URL + "/api/user/refreshtoken",
+  LOGOUT: BAISIC_URL + "/api/user/logout",
+  USERINFO: "/account/userinfo/",
+  LOGIN: "http://3.36.95.15:8080/api/user/login",
+  TOKEN_OBTAIN: "/account/token/obtain/",
+  TOKEN_BLACKLIST: "/account/token/blacklist/",
+  FEED_NEW: "http://3.36.95.15:8080/api/board",
+};
+export default API_URLS;

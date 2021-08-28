da74a4bfcef79da519360b4950082d58d805c0d9
seol <shs00421@gmail.com>
2021-08-28 16:33:01 +0900

    feat: 구글 소셜로그인 구현 (#28)

--- frontend/src/components/Login/LoginForm/PLoginForm.jsx
+++ frontend/src/components/Login/LoginForm/PLoginForm.jsx
@@ -7,9 +7,16 @@ function PLoginForm() {
       `${process.env.REACT_APP_API_URL}/oauth2/authorization/kakao`
     );
   };
+  const googleLogin = () => {
+    window.location.replace(
+      `${process.env.REACT_APP_API_URL}/oauth2/authorization/google`
+    );
+  };
   return (
     <S.FormContainer>
       <S.LoginButton onClick={kakaoLogin}>카카오 로그인</S.LoginButton>
+      <br />
+      <S.LoginButton onClick={googleLogin}>구글 로그인</S.LoginButton>
     </S.FormContainer>
   );
 }

c0c22204c955cee616eaa375cbffd4306415a8f0
seol <shs00421@gmail.com>
2021-09-02 22:27:15 +0900

    test: 모달 컴포넌트화 테스트 (#60)

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import TestModal from "../../components/Common/Modal/TestModal";
 import NavBar from "../../components/Common/NavBar";
 
 export default function Main() {
+  const [modalSwitch, setModalSwitch] = useState(false);
+
+  const onClick = () => {
+    setModalSwitch(true);
+  };
+
   return (
     <>
       <NavBar />
       <div>main</div>
+      <button onClick={onClick}>test</button>
+      {modalSwitch ? <TestModal setModalSwitch={setModalSwitch} /> : null}
     </>
   );
 }

717b1155c0e9db25c0fd23e13844b3308e2038b1
rkdud007 <gayeongparkk@gmail.com>
2021-09-23 15:18:09 +0900

    feat: 비즈 페이징  ( #142)

--- frontend/src/components/BProfile/BReviewBox/PBReviewBox.jsx
+++ frontend/src/components/BProfile/BReviewBox/PBReviewBox.jsx
@@ -3,18 +3,28 @@ import BProfileFeed from "../../Common/FeedBox/BProfileFeed";
 
 import * as S from "../style";
 
-export default function PBReviewBox({ reviewlist }) {
+export default function PBReviewBox({ reviewlist, handlepage, paging }) {
   return (
     <>
       <S.BoxTitle>후기 모아보기</S.BoxTitle>
       {reviewlist.length > 0 ? (
-        reviewlist.map((reviewEle) => (
-          <BProfileFeed
-            key={reviewEle.id}
-            state="review"
-            feedInfo={reviewEle}
-          />
-        ))
+        <>
+          {reviewlist.map((reviewEle) => (
+            <BProfileFeed
+              key={reviewEle.id}
+              state="review"
+              feedInfo={reviewEle}
+            />
+          ))}
+          <S.ListPaging>
+            {paging.first ? null : (
+              <p onClick={async () => await handlepage("PREV")}>이전</p>
+            )}
+            {paging.last ? null : (
+              <p onClick={async () => await handlepage("NEXT")}>다음</p>
+            )}
+          </S.ListPaging>
+        </>
       ) : (
         <S.IfNoneWordCont>
           <p>아직 후기가 작성되지 않았습니다.</p>
--- frontend/src/components/BProfile/BReviewBox/index.jsx
+++ frontend/src/components/BProfile/BReviewBox/index.jsx
@@ -5,14 +5,35 @@ import PBReviewBox from "./PBReviewBox";
 
 export default function BReviewBox({ businessId }) {
   const [reviewlist, setReviewlist] = useState([]);
+  const [page, setPage] = useState(1);
+  const [paging, setPaging] = useState({
+    first: "",
+    last: "",
+  });
   const getReviewlist = async () => {
-    await reviewListGetApi(businessId, 1)
-      .then((res) => setReviewlist(res.data.content))
+    await reviewListGetApi(businessId, page)
+      .then((res) => {
+        setReviewlist(res.data.content);
+        setPaging({ first: res.data.first, last: res.data.last });
+      })
       .catch((err) => console.log(err));
+  };
+  const handlepage = async (order) => {
+    if (order === "PREV") {
+      setPage(page - 1);
+    } else if (order === "NEXT") {
+      setPage(page + 1);
+    }
   };
   useEffect(() => {
     getReviewlist();
-  }, []);
+  }, [page]);
 
-  return <PBReviewBox reviewlist={reviewlist} />;
+  return (
+    <PBReviewBox
+      reviewlist={reviewlist}
+      handlepage={handlepage}
+      paging={paging}
+    />
+  );
 }

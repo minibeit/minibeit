69ec9d7ef1437914da9fdb992878482b9470334c
donup3 <donup08@naver.com>
2021-10-08 11:06:45 +0900

    feat: 후기 권한 수정, 로그인 response data 추가(#191)
    avatar 추가

--- backend/src/main/java/com/minibeit/user/domain/repository/UserRepositoryCustom.java
+++ backend/src/main/java/com/minibeit/user/domain/repository/UserRepositoryCustom.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepositoryCustom {
+    Optional<User> findByOauthIdWithAvatar(String oauthId);
+
     List<User> findAllInBusinessProfile(Long businessProfileId);
 
     Optional<User> findByIdWithSchool(Long userId);

adbba0b68d5e62f63dbbbb114b0d264b23675d64
Gyuchool <balance0591@naver.com>
2021-10-11 15:13:20 +0900

    test: 학교 리스트 조회 테스트(#214)

---
+++ backend/src/test/java/com/minibeit/school/service/SchoolServiceTest.java
@@ -0,0 +1,65 @@
+package com.minibeit.school.service;
+
+import com.minibeit.post.dto.PostResponse;
+import com.minibeit.school.domain.School;
+import com.minibeit.school.domain.SchoolRepository;
+import com.minibeit.school.dto.SchoolResponse;
+import com.minibeit.security.userdetails.CustomUserDetails;
+import com.minibeit.user.service.exception.SchoolNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@DisplayName("학교 조회 테스트")
+class SchoolServiceTest {
+
+    @Autowired
+    private SchoolRepository schoolRepository;
+    @Autowired
+    private SchoolService schoolService;
+
+    private School kSchool, kSchool2;
+
+    @BeforeEach
+    void set(){
+        initSchool();
+    }
+    private void initSchool() {
+        School school1 = School.builder().name("고려대").build();
+        kSchool = schoolRepository.save(school1);
+
+        School school2 = School.builder().name("고구려대").build();
+        kSchool2 = schoolRepository.save(school2);
+
+    }
+
+    @Test
+    @DisplayName("학교 리스트 조회 - 성공")
+    void getList() {
+
+        //'고'로 시작하는 학교
+        List<SchoolResponse.GetList> kSchoolList = schoolService.getList("고");
+        assertThat(kSchoolList.get(0).getName()).isEqualTo(kSchool.getName());
+        assertThat(kSchoolList.get(1).getName()).isEqualTo(kSchool2.getName());
+
+        //고려대 조회
+        List<SchoolResponse.GetList> kSchoolOne = schoolService.getList("고려대");
+        assertThat(kSchoolOne.get(0).getName()).isEqualTo(kSchool.getName());
+
+        //없는 학교 조회
+        List<SchoolResponse.GetList> emptyList = schoolService.getList("홍익대");
+        assertThat(emptyList.size()).isEqualTo(0);
+    }
+
+}

96c261a5d09d5d75e48150897ef627bd4a2bb3b7
donup3 <donup08@naver.com>
2021-11-07 17:16:34 +0900

    feat: 안쓰는 class 정리(#261)

--- backend/src/main/java/com/minibeit/security/exception/InvalidRefreshTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.minibeit.security.exception;
-
-public class InvalidRefreshTokenException extends TokenException {
-    public InvalidRefreshTokenException() {
-        super();
-    }
-}
--- backend/src/main/java/com/minibeit/security/exception/RefreshTokenNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.minibeit.security.exception;
-
-public class RefreshTokenNotFoundException extends TokenException {
-    public RefreshTokenNotFoundException() {
-        super();
-    }
-}

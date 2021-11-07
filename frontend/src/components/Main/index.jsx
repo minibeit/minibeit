f9aea10d4561ad05bd59a8bbb5059b35bb6712b3
UFFETKK <soyoungan02@naver.com>
2021-11-07 17:55:29 +0900

    feat: 날짜변경시 설정한 정보가 변경되는 것을 알려주는 알림창(#264)

--- frontend/src/components/Main/index.jsx
+++ frontend/src/components/Main/index.jsx
@@ -4,7 +4,7 @@ import MainSlide from "./MainSlide";
 import {Link} from "react-scroll";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-import RemoveAccount from "../Common/Alert/RemoveAccount"
+import DateChange from "../Common/Alert/DateChange";
 
 
 function MainComponent () {
@@ -41,7 +41,7 @@ function MainComponent () {
 
       {/* 여기여기여기여기여깅겨ㅣ */}
       <button onClick={onClick}>알럿만들기</button>
-      {alertSwitch ? <RemoveAccount setAlertSwitch={setAlertSwitch}/>:null}
+      {alertSwitch ? <DateChange setAlertSwitch={setAlertSwitch}/>:null}
     </S.BackGround>
   );
 }

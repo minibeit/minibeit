2b36d2d1cbb0ae5a87582e3aac8a7a692ef59ddf
UFFETKK <soyoungan02@naver.com>
2021-11-09 00:14:29 +0900

    feat: 상세내용만 수정가능 알림창(#264)

--- frontend/src/components/Main/index.jsx
+++ frontend/src/components/Main/index.jsx
@@ -4,7 +4,7 @@ import MainSlide from "./MainSlide";
 import {Link} from "react-scroll";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-import Recruting from "../Common/Alert/Recruting";
+import EditOnlyDetails from "../Common/Alert/EditOnlyDetails";
 
 
 function MainComponent () {
@@ -41,7 +41,7 @@ function MainComponent () {
 
       {/* 여기여기여기여기여깅겨ㅣ */}
       <button onClick={onClick}>알럿만들기</button>
-      {alertSwitch ? <Recruting setAlertSwitch={setAlertSwitch}/>:null}
+      {alertSwitch ? <EditOnlyDetails setAlertSwitch={setAlertSwitch}/>:null}
     </S.BackGround>
   );
 }

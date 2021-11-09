3f504b9a99aa93e6274e613fbe9ee06abf6a2587
UFFETKK <soyoungan02@naver.com>
2021-11-10 00:04:44 +0900

    feat: 이용약관 알림창(#264)

---
+++ frontend/src/components/Common/Alert/Conditions/style.jsx
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+
+export const AlertBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.3);
+  z-index: 99;
+`;
+export const AlertBox = styled.div`
+  transform: translate(-50%, -50%);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  background-color: white;
+  width: 40rem;
+  height: 40rem;
+  border-radius: 35px;
+`;
+
+export const AlertContent = styled.div`
+  transform: translate(-50%, -20%);
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  justify-content: center;
+  align-items: left;
+
+  & > div:first-child { 
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: baseline;
+    width: 35rem;
+  }
+
+  & > div > p:first-child {
+    font-size: 30px;
+    text-align: left;
+    font-weight: 700;
+  }
+
+  & > div > div:nth-child(2) {
+    font-size: 5rem;
+  }
+
+  & > div:nth-child(2) {
+    position: relative;
+    left: 50%;
+    transform: translate(-50%);
+    background-color: #F9F9F9;
+    border-radius: 17px;
+    font-size: 15px;
+    line-height: 20px;
+    width: 35rem;
+    height: 30rem;
+
+  }
+  & > div:nth-child(2) > p:first-child {
+    margin: 15px;
+    font-size: 15px;
+    text-align: left;
+    line-height: 23px;
+    width: 33rem;
+    height: 29rem;
+    overflow-y: scroll;
+    overflow-x: auto;
+    font-weight: 400;
+    ::-webkit-scrollbar {
+      width: 6px;
+    }
+    ::-webkit-scrollbar-track {
+      background-color: transparent;
+    }
+    ::-webkit-scrollbar-thumb {
+      border-radius: 3px;
+      background-color: gray;
+    }
+    ::-webkit-scrollbar-button {
+      width: 0;
+      height: 0;
+}
+  }
+
+`;
--- frontend/src/components/Main/index.jsx
+++ frontend/src/components/Main/index.jsx
@@ -4,7 +4,7 @@ import MainSlide from "./MainSlide";
 import {Link} from "react-scroll";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-import PersonalInformation from "../Common/Alert/PersonalInformation";
+import Conditions from "../Common/Alert/Conditions";
 
 
 
@@ -49,7 +49,7 @@ function MainComponent () {
       {/* 여기여기여기여기여깅겨ㅣ */}
       <button onClick={onClick}>알럿만들기</button>
       
-      {alertSwitch ? <PersonalInformation setAlertSwitch={setAlertSwitch}/>:null}
+      {alertSwitch ? <Conditions setAlertSwitch={setAlertSwitch}/>:null}
      
     </S.BackGround>
   );

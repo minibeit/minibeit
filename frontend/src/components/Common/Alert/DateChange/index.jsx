c65b5ebff18830b872baf5b327e0947e75e43822
UFFETKK <soyoungan02@naver.com>
2021-11-20 21:17:02 +0900

    feat: 날짜변경 알림창 배경선택시 꺼지게하기 (#308)

--- frontend/src/components/Common/Alert/DateChange/index.jsx
+++ frontend/src/components/Common/Alert/DateChange/index.jsx
@@ -6,14 +6,10 @@ import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 // 날짜변경시 설정한 정보가 변경되는것을 알려주는
 
-export default function DateChange ({setResetAlert, resetOk}) {
-  const closeAlert = () => {
-    setResetAlert(false);
-  };
-
+export default function DateChange ({setResetAlert, setResetAgree}) {
   return (
     <Portal>
-      <S.AlertBackground>
+      <S.AlertBackground onClick={(e)=>e.target===e.currentTarget && setResetAlert(false)}>
         <S.AlertBox>
           <S.AlertContent>
             <ErrorOutlineIcon  sx={{ fontSize: 40 , color: "#0642FF"}} />
@@ -21,8 +17,10 @@ export default function DateChange ({setResetAlert, resetOk}) {
             <span>정말로 날짜를 변경하시겠어요?</span>
             </p>
             <div>
-              <S.GrayButton onClick={closeAlert}>아니오, 돌아갈래요</S.GrayButton>
-              <S.BlueButton onClick={resetOk}>네, 날짜를 변경할래요</S.BlueButton>
+              <S.GrayButton onClick={() => setResetAlert(false)}>아니오, 돌아갈래요</S.GrayButton>
+              <S.BlueButton onClick={() => {
+                    setResetAlert(false);
+                    setResetAgree(true)}}>네, 날짜를 변경할래요</S.BlueButton>
             </div>
           </S.AlertContent>
         </S.AlertBox>

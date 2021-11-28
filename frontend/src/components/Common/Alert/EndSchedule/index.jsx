65e1e5af96ae43536f36a55d2a47bb150437ac84
UFFETKK <soyoungan02@naver.com>
2021-11-28 17:59:56 +0900

    feat: 비즈니스 프로필 관련 스타일 수정 완료(#327)

--- frontend/src/components/Common/Alert/EndSchedule/index.jsx
+++ frontend/src/components/Common/Alert/EndSchedule/index.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import Portal from "../Portal";
 import * as S from "./style";
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import { ReactComponent as InfoIcon } from "../../../../svg/경고.svg";
 
 // 일정이 끝났다고 알려주는 알림창
 
-export default function EndSchedule({setEndAlert, changeFeedData}) {
+export default function EndSchedule({ setEndAlert, changeFeedData }) {
   const clickOutside = (e) => {
-    e.target===e.currentTarget && 
-    setEndAlert(0); 
+    e.target === e.currentTarget && setEndAlert(0);
     document.querySelector("body").removeAttribute("style");
   };
-  const clickBtn =() => {
+  const clickBtn = () => {
     setEndAlert(0);
-    changeFeedData('완료된 모집공고');
+    changeFeedData("완료된 모집공고");
     document.querySelector("body").removeAttribute("style");
   };
   return (
     <Portal>
-      <S.AlertBackground onClick={(e)=>clickOutside(e)}>
+      <S.AlertBackground onClick={(e) => clickOutside(e)}>
         <S.AlertBox>
           <S.AlertContent>
-            <ErrorOutlineIcon  sx={{ fontSize: 40}} />
-            <p>해당 모집 공고의<br/><span>일정이 종료</span>되었어요.</p>
+            <InfoIcon />
+            <p>
+              해당 모집 공고의
+              <br />
+              <span>일정이 종료</span>되었어요.
+            </p>
             <button onClick={clickBtn}>닫기</button>
           </S.AlertContent>
         </S.AlertBox>
       </S.AlertBackground>
     </Portal>
-  )}
+  );
+}

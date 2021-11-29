84c0fe6dc3334d64635e9ed6ee9d5de50f811c5a
UFFETKK <soyoungan02@naver.com>
2021-11-30 06:08:09 +0900

    feat: 메인 이동하게 만들기 (#337)

--- frontend/src/components/Main/index.jsx
+++ frontend/src/components/Main/index.jsx
@@ -1,38 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 import MainSlide from "./MainSlide";
-import {Link} from "react-scroll";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { Link } from "react-scroll";
+import { useHistory } from "react-router";
+import { useRecoilValue } from "recoil";
+import { userState } from "../../recoil/userState";
+import CreateAuthModal from "../Common/Modal/CreateAuthModal";
 
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
+export default function MainComponent() {
+  const history = useHistory();
+  const user = useRecoilValue(userState);
+  const isLogin = user.isLogin;
+  const [modalSwitch, setModalSwitch] = useState(false);
 
+  const goApply = () => {
+    if (isLogin) {
+      history.push("/apply");
+    } else {
+      let value = window.confirm("이용하려면 로그인 먼저 해주세요!");
+      if (value) {
+        setModalSwitch(true);
+      }
+    }
+  };
 
-export default function MainComponent () {
+  const goRecruit = () => {
+    if (isLogin) {
+      history.push("/recruit");
+    } else {
+      let value = window.confirm("이용하려면 로그인 먼저 해주세요!");
+      if (value) {
+        setModalSwitch(true);
+      }
+    }
+  };
   return (
     <S.BackGround>
       <S.MainJumbotron>
-          <p>초단기 구인구직을 위한<br /> 딱 맞는 퍼즐, 미니바이트</p>
-          <div>
-            <Link to="1" spy={true} smooth={true}><S.WhiteButton>참여하기</S.WhiteButton></Link>
-            <Link to="2" spy={true} smooth={true}><S.WhiteButton>모집하기</S.WhiteButton></Link>
-          </div>
-          <p>일단 둘러볼래요</p>
-          <Link to="3" spy={true} smooth={true}><KeyboardArrowDownIcon color="disabled"/></Link>
+        <p>
+          초단기 구인구직을 위한
+          <br /> 딱 맞는 퍼즐, 미니바이트
+        </p>
+        <div>
+          <Link to="1" spy={true} smooth={true}>
+            <S.WhiteButton>참여하기</S.WhiteButton>
+          </Link>
+          <Link to="2" spy={true} smooth={true}>
+            <S.WhiteButton>모집하기</S.WhiteButton>
+          </Link>
+        </div>
+        <p>일단 둘러볼래요</p>
+        <Link to="3" spy={true} smooth={true}>
+          <KeyboardArrowDownIcon color="disabled" />
+        </Link>
       </S.MainJumbotron>
       <S.Section id="1">
-          <S.BlueButton>참여하기</S.BlueButton>
-          <p>가까운 위치에, 남는 시간에 <br/> 간편하게 지원하기 </p>
-          <MainSlide/>
+        <S.BlueButton onClick={goApply}>참여하기</S.BlueButton>
+        <p>
+          가까운 위치에, 남는 시간에 <br /> 간편하게 지원하기{" "}
+        </p>
+        <MainSlide />
       </S.Section>
+      {modalSwitch ? <CreateAuthModal setModalSwitch={setModalSwitch} /> : null}{" "}
       <S.Section id="2">
-          <S.BlueButton>모집하기</S.BlueButton>
-          <p>가장 쉽고 빠르게 <br/> 원하는 스케줄로 모집하기 </p>
-          <MainSlide/>
+        <S.BlueButton onClick={goRecruit}>모집하기</S.BlueButton>
+        <p>
+          가장 쉽고 빠르게 <br /> 원하는 스케줄로 모집하기{" "}
+        </p>
+        <MainSlide />
       </S.Section>
-      <S.LastJumbo id="3">
-      <p>세상에 없었던<br/>새로운 <span>구인구직</span> 서비스,<br/>미니바이트</p>
-      </S.LastJumbo>
+      <div id="3">
+        <S.LastJumbo>
+          <p>
+            세상에 없었던
+            <br />
+            새로운 <span>구인구직</span> 서비스,
+            <br />
+            미니바이트
+          </p>
+        </S.LastJumbo>
+      </div>
     </S.BackGround>
   );
 }
-
--- frontend/src/components/Main/style.jsx
+++ frontend/src/components/Main/style.jsx
@@ -1,148 +1,137 @@
 import styled from "styled-components";
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 export const BackGround = styled.div`
-width: 100vw;
-background-color: #F9F9F9;
-padding-bottom: 10rem;
-overflow: hidden;
-max-width: 100%;
+  width: 100vw;
+  background-color: #f9f9f9;
+  max-width: 100%;
+  overflow-x: hidden;
+  & > div:last-child {
+    padding: 4rem 12rem;
+    height: 100vh;
+  }
 `;
 
 export const MainJumbotron = styled.div`
-position: relative;
-width: 100vw;
-height: 100vh;
-background-color: #FFFFFF;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-flex-wrap: wrap;
-
-& > p:first-child {
   position: relative;
-  height: 140px;
-  font-size: 40px;
-  font-weight: 700;
-  line-height: 60px;
-  color: #000000;
-  text-align: center;
-}
-
-& > div:nth-child(2) {
+  width: 100vw;
+  height: 100vh;
+  background-color: #ffffff;
   display: flex;
-  flex-direction: row;
-  gap: 50px;
-}
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
 
-& > p:nth-child(3) {
-  width: 140px;
-  height:27px;
-  font-size: 14px;
-  color: #D7D7D7;
-  line-height: 27px;
-  text-align: center;
-  text-decoration: underline;
-  margin: 50px 0 70px 0;
-}
+  & > p:first-child {
+    position: relative;
+    height: 140px;
+    font-size: 40px;
+    font-weight: 700;
+    line-height: 60px;
+    color: #000000;
+    text-align: center;
+  }
+
+  & > div:nth-child(2) {
+    display: flex;
+    flex-direction: row;
+    gap: 50px;
+  }
+
+  & > p:nth-child(3) {
+    width: 140px;
+    height: 27px;
+    font-size: 14px;
+    color: #d7d7d7;
+    line-height: 27px;
+    text-align: center;
+    text-decoration: underline;
+    margin: 50px 0 70px 0;
+  }
 `;
 
 const Button = styled.button`
-position: relative;
-margin-top: 30px;
-width: 120px;
-height: 50px;
-font-size: 20px;
-font-weight: bold;
-line-height: 50px;
-box-shadow: 10px 10px 30px rgba(189, 189, 189, 0.2);
-border-radius: 42.5px;
+  position: relative;
+  margin-top: 30px;
+  width: 120px;
+  height: 50px;
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 50px;
+  box-shadow: 10px 10px 30px rgba(189, 189, 189, 0.2);
+  border-radius: 42.5px;
+  cursor: pointer;
 `;
 
 export const WhiteButton = styled(Button)`
-color: #0642FF;
-border: 1px solid #FFFFFF;
-background: #FFFFFF;
-margin-top: 30px;
+  color: #0642ff;
+  border: 1px solid #ffffff;
+  background: #ffffff;
+  margin-top: 30px;
 `;
 
 export const BlueButton = styled(Button)`
-color: #FFFFFF;
-border: 1px solid #0642FF;
-background: #0642FF;
+  color: #ffffff;
+  border: 1px solid #0642ff;
+  background: #0642ff;
 `;
-
 
 export const Section = styled.div`
-position: relative;
-width: 70vw;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: left;
-flex-wrap: wrap;
-gap: 20px;
-margin: 70px 0;
-left: 150px;
+  position: relative;
+  padding: 4rem 12rem;
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  gap: 0.6rem;
 
-& > button:first-child {
+  & > button:first-child {
+  }
 
-}
-
-& > p:nth-child(2) {
-  font-size: 20px;
-  font-weight: 800;
-  line-height: 40px;
-  color: #000000;
-  padding-left: 20px;
-}
-
-& > div:last-child {
- left: 50px;
-}
-
+  & > p:nth-child(2) {
+    font-size: 20px;
+    font-weight: 800;
+    line-height: 40px;
+    color: #000000;
+    margin-left: 1.5rem;
+  }
 `;
-
 
 export const LastJumbo = styled.div`
-position: relative;
-width: 700px;
-height: 450px;
-left: 50%;
-transform: translateX(-50%);
-box-shadow: 10px 10px 30px rgba(189, 189, 189, 0.2);
-border-radius: 30px;
-background-color: #FFFFFF;
-
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-flex-wrap: wrap;
-text-align: center;
-
-
-& > p:first-child {
   position: relative;
-  height: 200;
-  font-size: 40px;
-  font-weight: bold;
-  line-height: 90px;
-  color: #000000;
-}
+  margin: auto;
+  width: 700px;
+  height: 450px;
+  box-shadow: 10px 10px 30px rgba(189, 189, 189, 0.2);
+  border-radius: 30px;
+  background-color: #ffffff;
 
-& > p > span:nth-child(2) {
-  position: relative;
-  text-emphasis-style: filled #0642FF;
-  -webkit-text-emphasis: filled #0642FF;
-}
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  text-align: center;
+
+  & > p:first-child {
+    position: relative;
+    height: 200;
+    font-size: 40px;
+    font-weight: bold;
+    line-height: 90px;
+    color: #000000;
+  }
+
+  & > p > span:nth-child(2) {
+    position: relative;
+    text-emphasis-style: filled #0642ff;
+    -webkit-text-emphasis: filled #0642ff;
+  }
 `;
-
-
-
-
 
 // slider
 export const Container = styled.div`
@@ -150,7 +139,6 @@ export const Container = styled.div`
   width: 85vw;
   overflow: hidden;
 `;
-
 
 export const ImageContainer = styled.div`
   position: relative;
@@ -165,23 +153,22 @@ export const ImageContainer = styled.div`
 export const Image = styled.img`
   position: absolute;
   max-width: 100px;
-  max-height:100px;
+  max-height: 100px;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
 `;
 
-
 const ArrowButton = styled.button`
   position: absolute;
-  top: calc(50% - 50px); 
-  padding: 0; 
-  width: 30px; 
-  height: 30px; 
-  line-height: 1; 
-  border: none; 
-  background: none; 
-  outline: none; 
+  top: calc(50% - 50px);
+  padding: 0;
+  width: 30px;
+  height: 30px;
+  line-height: 1;
+  border: none;
+  background: none;
+  outline: none;
   cursor: pointer;
 `;
 
@@ -194,19 +181,19 @@ export const NextButton = styled(ArrowButton)`
 `;
 
 export const PrevIcon = styled(ArrowBackIosIcon)`
-font-size: 22px; 
-color: gray; 
-&:focus, &:hover {
-  color: #666; }
-`; 
-
-export const NextIcon = styled(ArrowForwardIosIcon)` 
-font-size: 22px; 
-color: gray; 
-&:focus, &:hover {
-  color: #666; }
+  font-size: 22px;
+  color: gray;
+  &:focus,
+  &:hover {
+    color: #666;
+  }
 `;
 
-
-
-
+export const NextIcon = styled(ArrowForwardIosIcon)`
+  font-size: 22px;
+  color: gray;
+  &:focus,
+  &:hover {
+    color: #666;
+  }
+`;

5ff4882df0428973db5ecda361024e4e9fc1abb8
UFFETKK <soyoungan02@naver.com>
2021-12-01 09:02:28 +0900

    fix: signup finish 스타일 수정(#337)

--- frontend/src/components/SignUp/SignupFinish/index.jsx
+++ frontend/src/components/SignUp/SignupFinish/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router";
-import CloseIcon from "@mui/icons-material/Close";
-
+import { ReactComponent as CloseIcon } from "../../../svg/엑스.svg";
 import Portal from "../../Common/Modal/Portal";
 import { PVImg } from "../../Common";
 
@@ -29,7 +28,9 @@ export default function SignupFinish() {
           </S.ModalHeader>
           <S.ModalContent>
             <S.MessageBox>
-              <div>🎉</div>
+              <div>
+                <img src="/images/party.png" alt="파티" />
+              </div>
               <p>{inputData.name}님,</p>
               <p>미니바이트에 가입하신 것을 환영합니다!</p>
             </S.MessageBox>
@@ -45,11 +46,7 @@ export default function SignupFinish() {
               <p>
                 {inputData.year}.{inputData.month}.{inputData.date}
               </p>
-              <p>
-                {inputData.phoneNum1}
-                {inputData.phoneNum2}
-                {inputData.phoneNum3}
-              </p>
+              <p>{inputData.phoneNum}</p>
               <p>{inputData.job}</p>
             </S.DataBox>
             <p>닫기 버튼을 누르시면 홈으로 돌아가요</p>

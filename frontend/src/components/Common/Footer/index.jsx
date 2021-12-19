1866b723b702cd2b303829182d10d352fbcb7c63
UFFETKK <soyoungan02@naver.com>
2021-12-19 21:26:31 +0900

    fix: 풋터 디자인 수정(#393)

--- frontend/src/components/Common/Footer/index.jsx
+++ frontend/src/components/Common/Footer/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import * as S from "./style";
 import Conditions from "../Alert/Conditions";
 import PersonalInformation from "../Alert/PersonalInformation";
+import { ReactComponent as BeitIcon } from "../../../svg/beitIcon.svg";
 
 export default function Footer() {
   const [alert, setConditionsAlert] = useState(0);
@@ -13,30 +14,35 @@ export default function Footer() {
       )}
       <div>
         <div>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.facebook.com/%EB%AF%B8%EB%8B%88%EB%B0%94%EC%9D%B4%ED%8A%B8-minibeit-101946832298611"
-          >
-            <img src="/images/facebook.png" alt="facebookIcon" />
-          </a>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.instagram.com/_minibeit_/?hl=ko"
-          >
-            <img src="/images/insta.png" alt="instaIcon" />
-          </a>
+          <div>
+            <BeitIcon /> Minibeit
+          </div>
+          <div>기업소개</div>
+          <div onClick={() => setConditionsAlert(1)}>이용약관</div>
+          <div onClick={() => setConditionsAlert(2)}>개인정보 처리방침</div>
+          <div>고객센터</div>
+          <div>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.facebook.com/%EB%AF%B8%EB%8B%88%EB%B0%94%EC%9D%B4%ED%8A%B8-minibeit-101946832298611"
+            >
+              <img src="/images/facebook.png" alt="facebookIcon" />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.instagram.com/_minibeit_/?hl=ko"
+            >
+              <img src="/images/insta.png" alt="instaIcon" />
+            </a>
+          </div>
         </div>
         <div>
-          <div>
-            <div onClick={() => setConditionsAlert(1)}>이용약관</div>
-            <div onClick={() => setConditionsAlert(2)}>개인정보처리방침</div>
-          </div>
           <p>미니바이트 | 서울시 동대문구 제기로6길 37-2 103호</p>
           <p>Produced by CLMOI - Korea University StartUp Team</p>
+          <p>ⓒminibeit</p>
         </div>
-        <p>ⓒminibeit</p>
       </div>
     </S.FooterContainer>
   );

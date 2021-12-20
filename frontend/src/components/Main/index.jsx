c995425b83b42cb323e5bfe226a0a46178900194
UFFETKK <soyoungan02@naver.com>
2021-12-20 23:01:10 +0900

    feat: 스크롤 투명도적용(#393)

--- frontend/src/components/Main/index.jsx
+++ frontend/src/components/Main/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./style";
 import { Link } from "react-scroll";
 import { useHistory } from "react-router";
@@ -7,6 +7,8 @@ import { userState } from "../../recoil/userState";
 import CreateAuthModal from "../Common/Modal/CreateAuthModal";
 
 import { ReactComponent as ArrowDown } from "../../svg/체크.svg";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function MainComponent() {
   const history = useHistory();
@@ -20,6 +22,10 @@ export default function MainComponent() {
       setModalSwitch(true);
     }
   };
+
+  useEffect(() => {
+    AOS.init({ duration: 1500 });
+  });
 
   return (
     <S.BackGround>
@@ -42,7 +48,7 @@ export default function MainComponent() {
           </Link>
         </div>
       </S.MainBox>
-      <S.MainBox2 id="1">
+      <S.MainBox2 id="1" data-aos="fade-in">
         <div>
           <div>
             <span>가까운 위치</span>에, <span>남는 시간</span>에 <br /> 간편하게
@@ -54,7 +60,7 @@ export default function MainComponent() {
         </div>
         <img src="/images/main.png" alt="mainImg" />
       </S.MainBox2>
-      <S.MainBox3 id="2">
+      <S.MainBox3 id="2" data-aos="fade-in">
         <div>
           <div>
             가장 쉽고 빠르게 <br /> <span>원하는 스케줄</span>로 모집하기
@@ -63,7 +69,7 @@ export default function MainComponent() {
         </div>
         <img src="/images/main2.png" alt="mainImg" />
       </S.MainBox3>
-      <S.LastBox id="3">
+      <S.LastBox id="3" data-aos="fade-in">
         <div>
           <div>
             세상에 없었던

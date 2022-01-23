ec32bb8561011798d98e86b923ace22dfee00ef4
donup3 <donup08@naver.com>
2022-01-23 13:33:16 +0900

    feat: dev,prod 배포 스크립트 분리(#497)

--- backend/scripts/prod/start.sh
+++ backend/scripts/prod/start.sh
@@ -4,15 +4,15 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITORY=/home/ec2-user/app/dev
+REPOSITORY=/home/ec2-user/app
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/prod"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/prod
 
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/prod/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
@@ -26,6 +26,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-dev.yml,/home/ec2-user/app/application-aws.yml \
-    -Dspring.profiles.active=dev,$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-prod.yml,/home/ec2-user/app/application-aws.yml \
+    -Dspring.profiles.active=prod,$IDLE_PROFILE \
+    $JAR_NAME > $REPOSITORY/prod/nohup.out 2>&1 &

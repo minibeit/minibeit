abdb9f4049c32721b1b44e437a0a06e432ceef1f
UFFETKK <soyoungan02@naver.com>
2022-01-29 04:51:34 +0900

    fix: 테스트(#500)

--- frontend/src/components/SignUp/SignupFinish/index.jsx
+++ frontend/src/components/SignUp/SignupFinish/index.jsx
@@ -21,17 +21,15 @@ export default function SignupFinish() {
           </S.CloseModalBtn>
         </S.ModalHeader>
         <S.ModalContent>
-          <S.MessageBox>
-            <S.ImgBox>
-              {!inputData.avatar === null ? (
-                <PVImg img={inputData.avatar} />
-              ) : (
-                <img src="/images/팡파레아이콘.png" alt="파티" />
-              )}
-            </S.ImgBox>
-            <p>{inputData.name}님,</p>
-            <p>미니바이트에 가입하신 것을 환영합니다!</p>
-          </S.MessageBox>
+          <S.ImgBox>
+            {!inputData.avatar === null ? (
+              <PVImg img={inputData.avatar} />
+            ) : (
+              <img src="/images/팡파레아이콘.png" alt="파티" />
+            )}
+          </S.ImgBox>
+          <p>{inputData.name}님,</p>
+          <p>미니바이트에 가입하신 것을 환영합니다!</p>
           <button onClick={() => history.push("/")}>별말씀을요</button>
           <p>닫기 버튼을 누르시면 홈으로 돌아가요</p>
         </S.ModalContent>
